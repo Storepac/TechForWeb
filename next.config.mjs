492451102d2b2dc4ad48156214a4e186ece9b75f
Anderson <38814895+Storepac@users.noreply.github.com>
2025-07-02 23:14:52 -0300

    build

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Configurações para melhorar o deploy no Vercel
-  output: 'standalone',
-  experimental: {
-    optimizeCss: true,
-  },
   // Configurações de cache e performance
   compress: true,
   poweredByHeader: false,
